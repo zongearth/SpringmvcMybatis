58b20d1d5c882415c26606276603163b07562f40
ZongJustyouGit <592391600@qq.com>
2017-04-01 23:16:25 +0800

    修改

--- src/main/java/com/tiger/kxz/controller/TestController.java
+++ src/main/java/com/tiger/kxz/controller/TestController.java
@@ -1,27 +1,31 @@
 package com.tiger.kxz.controller;
 
-import com.tiger.kxz.dao.OrdersMapper;
-import com.tiger.kxz.entity.Orders;
+import com.tiger.kxz.dao.UserMapper;
+import com.tiger.kxz.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Created by Administrator on 2016/12/13.
+ * 测试类
  */
 @Controller
 @RequestMapping("/testController")
 public class TestController {
 
     @Autowired
-    private OrdersMapper ordersMapper;
+    private UserMapper userMapper;
 
     @RequestMapping(value = "/view", method = RequestMethod.GET)
     public String view(){
-        String id = "I0000025";
-        Orders orders = ordersMapper.selectByPrimarykey(id);
-        System.out.println(orders.getORDER_NO() + orders.getGEN_INSTTU_CODE());
+        Long id = 0000025L;
+        User user = new User();
+        user.setID(id);
+        user.setLoginName("admin");
+        user.setLoginPassWord("123");
+        user.setName("小明");
+        userMapper.insert(user);
         return "index";
     }
 
